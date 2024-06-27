f7d10733ed1d3c36ca8b0e242d8e28545b218e8f
Mieboymesias <161265927+Mieboymesias@users.noreply.github.com>
2024-06-27 12:32:42 +0800

    BRGY 2024
    BRGY 2024

---
+++ data/NONECONDITION_8.js
@@ -0,0 +1,9 @@
+var json_NONECONDITION_8 = {
+"type": "FeatureCollection",
+"name": "NONECONDITION_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "photo": "D:\\Brgy. Facilities Inventory 2024\\NONE\\MABUA ES MULTI PURPOSE BUILDING.jpg", "filename": "MABUA ES MULTI PURPOSE BUILDING", "directory": "D:\\Brgy. Facilities Inventory 2024\\NONE", "altitude": null, "direction": null, "longitude": "125.44123975000001", "latitude": "9.810181861111111", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 125.44123975, 9.810181861111111 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "photo": "D:\\Brgy. Facilities Inventory 2024\\NONE\\NONOC MULTI PURPOSE BUILDING ELEMENTARY.jpg", "filename": "NONOC MULTI PURPOSE BUILDING ELEMENTARY", "directory": "D:\\Brgy. Facilities Inventory 2024\\NONE", "altitude": null, "direction": null, "longitude": "125.58116197222222", "latitude": "9.820447027777778", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 125.581161972222219, 9.820447027777778 ] } }
+]
+}
